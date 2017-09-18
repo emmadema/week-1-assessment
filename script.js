16d23647993672c1cfc740b652513d166b63b1f6
emmadema <emma.demango@gmail.com>
2017-09-17 20:29:26 -0600

    trying some JS

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+window.addEventListener("DOMContentLoaded", setBodyGradientOnLoad);
+
+function setBodyGradientOnLoad() {
+    var heightPx = window.outerHeight;
+    var gradientStop = Math.floor( heightPx * 0.3 );
+    var body = document.getElementsByTagName("body")[0];
+    body.style.backgroundImage = "linear-gradient(to bottom, #6283a1, #4b7193  " + gradientStop + "px, white " + gradientStop + "px, white)";
+}
