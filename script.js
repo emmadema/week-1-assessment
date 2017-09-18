8b4daf79477b92b797c5868108f2b36889220a36
emmadema <emma.demango@gmail.com>
2017-09-17 20:48:25 -0600

    Yay got it to work

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 window.addEventListener("DOMContentLoaded", setBodyGradientOnLoad);
 
 function setBodyGradientOnLoad() {
-    var heightPx = window.outerHeight;
+    var heightPx = window.innerHeight;
     var gradientStop = Math.floor( heightPx * 0.3 );
     var body = document.getElementsByTagName("body")[0];
     body.style.backgroundImage = "linear-gradient(to bottom, #6283a1, #4b7193  " + gradientStop + "px, white " + gradientStop + "px, white)";
