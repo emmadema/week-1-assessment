198fbf9bbd808ae04e04db8417daf49fe1725fab
emmadema <emma.demango@gmail.com>
2017-09-24 23:33:07 -0600

    remove JS

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //used to make the color appear on 1/3 of the page instead of 1/2 
 
 
-window.addEventListener("DOMContentLoaded", setBodyGradientOnLoad); // as the page uploads run this color
+//window.addEventListener("DOMContentLoaded", setBodyGradientOnLoad); // as the page uploads run this color
 
-function setBodyGradientOnLoad() {
-    var heightPx = window.innerHeight;
-    var gradientStop = Math.floor( heightPx * 0.3 ); // this is the 1/3
-    var body = document.getElementsByTagName("body")[0];
-    body.style.backgroundImage = "linear-gradient(to bottom, #6283a1, #4b7193  " + gradientStop + "px, white " + gradientStop + "px, white)";
-}
+//function setBodyGradientOnLoad() {
+    //var heightPx = window.innerHeight;
+   // var gradientStop = Math.floor( heightPx * 0.3 ); // this is the 1/3
+   // var body = document.getElementsByTagName("body")[0];
+   // body.style.backgroundImage = "linear-gradient(to bottom, #6283a1, #4b7193  " + gradientStop + "px, white " + gradientStop + "px, white)";
+//
